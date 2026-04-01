83d0a98ca3bc7e3828f044b3decb1fc40642a6b3
Ethan Larson <ealarson@outlook.com>
2026-04-01 16:08:12 -0500

    Fixed text sizing for page header

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -6,7 +6,7 @@
 // --- Asset version (cache-busting) ---
 // Bump on deploy. Format: MAJOR.MINOR.PATCH
 // MAJOR = breaking; MINOR = new features; PATCH = fixes only
-window.ASSET_VERSION = "6.0.1";
+window.ASSET_VERSION = "6.0.2";
 
 // V5.0.0 First official release to Cadets!
 
