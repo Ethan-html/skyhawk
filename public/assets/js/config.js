dcb08e71f252b92e5109f5e62eefcccb0bde1a33
Ethan Larson <ealarson@outlook.com>
2026-04-21 17:14:03 -0500

    Added drill tests and updated calldown form

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -6,7 +6,7 @@
 // --- Asset version (cache-busting) ---
 // Bump on deploy. Format: MAJOR.MINOR.PATCH
 // MAJOR = breaking; MINOR = new features; PATCH = fixes only
-window.ASSET_VERSION = "6.0.4";
+window.ASSET_VERSION = "6.0.5";
 
 // V5.0.0 First official release to Cadets!
 
